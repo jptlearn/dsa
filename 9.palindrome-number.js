a71a6104bf127aac0f4d456b18c9a162c395e45d
jptlearn <jptcode500@gmail.com>
2025-01-27 23:55:27 +0545

    solve roman-to-integer.js

---
+++ 9.palindrome-number.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=9 lang=javascript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+
+  // convert to String
+  const str = x.toString();
+
+  // Use two pointers
+  let left = 0;
+  let right = str.length - 1;
+
+  // compare characters from both ends
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+// @lc code=end
