fb94013b9edd2516db02cc27cc31b3f9b401b0cc
jptlearn <jptcode500@gmail.com>
2025-01-31 23:30:08 +0545

    map for matching brackets at first

---
+++ 20.valid-parentheses.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=20 lang=javascript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  // Map for matching brackets
+  const brackets = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+};
+// @lc code=end
