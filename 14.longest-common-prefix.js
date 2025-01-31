84d4019f5f34281fc7590b974251f4be4ce7627b
jptlearn <jptcode500@gmail.com>
2025-01-31 23:11:47 +0545

    compare with the other strings and return final answer

--- 14.longest-common-prefix.js
+++ 14.longest-common-prefix.js
@@ -16,5 +16,16 @@ var longestCommonPrefix = function (strs) {
 
   // Take first string as references
   let prefix = strs[0];
+
+  // compare with other strings
+  for (let i = 1; i < strs.length; i++) {
+    // keep reducing prefix until it match
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
 };
 // @lc code=end
