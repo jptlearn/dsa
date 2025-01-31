c623359d4437820a43ca64091cfbad293116a55a
jptlearn <jptcode500@gmail.com>
2025-01-31 23:59:38 +0545

    compare if both lists have nodes and apply logic with while loop

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -21,5 +21,17 @@ var mergeTwoLists = function (list1, list2) {
   // Create dummy node to avoid edge cases
   let dummy = new ListNode(0);
   let current = dummy;
+
+  // While both lists have nodes
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
 };
 // @lc code=end
