f36a7705b1a82744f0285f5cfee9c3790471be8a
jptlearn <jptcode500@gmail.com>
2025-01-31 23:57:36 +0545

    create dummy and current node

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  // Create dummy node to avoid edge cases
+  let dummy = new ListNode(0);
+  let current = dummy;
+};
+// @lc code=end
